4adab9253ad199a734a06f11941d2e7a2fcbb900
Petr H <Petr.Hostalek@bsc-ideas.com>
2025-08-19 16:09:35 +0200

    APIv2: Initial release

--- active24/dns.go
+++ active24/dns.go
@@ -22,146 +22,214 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
 )
 
 type DnsRecordType string
 
 //goland:noinspection GoUnusedConst
 const (
-	DnsRecordTypeA     = DnsRecordType("a")
-	DnsRecordTypeAAAA  = DnsRecordType("aaaa")
-	DnsRecordTypeCAA   = DnsRecordType("caa")
-	DnsRecordTypeCNAME = DnsRecordType("cname")
-	DnsRecordTypeMX    = DnsRecordType("mx")
-	DnsRecordTypeNS    = DnsRecordType("ns")
-	DnsRecordTypeSRV   = DnsRecordType("srv")
-	DnsRecordTypeSSHFP = DnsRecordType("sshfp")
-	DnsRecordTypeTLSA  = DnsRecordType("tlsa")
-	DnsRecordTypeTXT   = DnsRecordType("txt")
+	DnsRecordTypeA     = DnsRecordType("A")
+	DnsRecordTypeAAAA  = DnsRecordType("AAAA")
+	DnsRecordTypeANAME = DnsRecordType("ANAME")
+	DnsRecordTypeCAA   = DnsRecordType("CAA")
+	DnsRecordTypeCNAME = DnsRecordType("CNAME")
+	DnsRecordTypeMX    = DnsRecordType("MX")
+	DnsRecordTypeSRV   = DnsRecordType("SRV")
+	DnsRecordTypeTXT   = DnsRecordType("TXT")
+	DnsRecordTypeCERT  = DnsRecordType("CERT")
+	DnsRecordTypeLOC   = DnsRecordType("LOC")
+	DnsRecordTypeSSHFP = DnsRecordType("SSHFP")
+	DnsRecordTypeTLSA  = DnsRecordType("TLSA")
+	DnsRecordTypeDS    = DnsRecordType("DS")
+	DnsRecordTypeNS    = DnsRecordType("NS")
 )
-
-type DnsDomainList []string
 
 // Dns provides a way to interact with DNS domains
 type Dns interface {
-	// List returns a list of domains which have DNS records managed by Active 24.
-	List() (DnsDomainList, ApiError)
-	// With returns interface to interact with DNS records in given domain
-	With(domain string) DnsRecordActions
+	// With returns interface to interact with DNS records in given service ID (domain)
+	With(domain string, serviceID int) DnsRecordActions
 }
 
 type DnsRecord struct {
-	HashId           *string `json:"hashId,omitempty"`
-	Type             *string `json:"type,omitempty"`
-	Ttl              int     `json:"ttl"`
-	Name             string  `json:"name"`
-	Port             *int    `json:"port,omitempty"`
-	Priority         *int    `json:"priority,omitempty"`
-	Target           *string `json:"target,omitempty"`
-	Weight           *int    `json:"weight,omitempty"`
-	NameServer       *string `json:"nameserver,omitempty"`
-	Ip               *string `json:"ip,omitempty"`
-	CaaValue         *string `json:"caaValue,omitempty"`
-	Flags            *int    `json:"flags,omitempty"`
-	Tag              *string `json:"tag,omitempty"`
-	Alias            *string `json:"alias,omitempty"`
-	MailServer       *string `json:"mailserver,omitempty"`
-	Algorithm        *int    `json:"algorithm,omitempty"`
-	FingerprintType  *int    `json:"fingerprintType,omitempty"`
-	CertificateUsage *int    `json:"certificateUsage,omitempty"`
-	Hash             *string `json:"hash,omitempty"`
-	MatchingType     *int    `json:"matchingType,omitempty"`
-	Selector         *int    `json:"selector,omitempty"`
-	Text             *string `json:"text,omitempty"`
+	Type     *string `json:"type,omitempty"`
+	ID       *int    `json:"id,omitempty"`
+	Name     string  `json:"name"`
+	Content  *string `json:"content,omitempty"`
+	Ttl      int     `json:"ttl"`
+	Priority *int    `json:"priority,omitempty"`
+	Port     *int    `json:"port,omitempty"`
+	Weight   *int    `json:"weight,omitempty"`
+}
+
+type DnsRecordPaginatedCollection struct {
+	CurrentPage  *int        `json:"currentPage,omitempty"`
+	TotalPages   *int        `json:"totalPages,omitempty"`
+	TotalRecords *int        `json:"totalRecords,omitempty"`
+	RowsPerPage  *int        `json:"rowsPerPage,omitempty"`
+	NextPageUrl  *string     `json:"nextPageUrl,omitempty"`
+	Data         []DnsRecord `json:"data,omitempty"`
 }
 
 // DnsRecordActions allows interaction with DNS records
 type DnsRecordActions interface {
 	// Create creates a new DNS record
-	Create(DnsRecordType, *DnsRecord) ApiError
+	Create(*DnsRecord) ApiError
 	// List lists all DNS records in this domain.
-	List() ([]DnsRecord, ApiError)
+	ListAll() ([]DnsRecord, ApiError)
+	// List lists DNS records of specified type or name in this domain.
+	List(DnsRecordType, string) ([]DnsRecord, ApiError)
+	// ListPage lists 1 page of DNS records of specified type or name in this domain.
+	ListPage(DnsRecordType, string, string, int) ([]DnsRecord, string, int, ApiError)
 	// Update updates an existing DNS record
-	Update(DnsRecordType, *DnsRecord) ApiError
-	// Delete removes single DNS record based on its hash ID
-	Delete(string) ApiError
+	Update(int, *DnsRecord) ApiError
+	// Delete removes single DNS record based on its ID
+	Delete(int) ApiError
 }
 
 type dns struct {
 	h helper
 }
 
-func (d *dns) List() (DnsDomainList, ApiError) {
-	resp, err := d.h.do(http.MethodGet, "dns/domains/v1", nil)
-	if err != nil {
-		return nil, apiErr(nil, err)
-	}
-	defer func(b io.ReadCloser) {
-		_ = b.Close()
-	}(resp.Body)
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, apiErr(resp, err)
-	}
-	doms := make([]string, 0)
-	err = json.Unmarshal(body, &doms)
-	if err != nil {
-		return nil, apiErr(resp, err)
-	}
-	return doms, apiErr(resp, nil)
-}
-
-func (d *dns) With(domain string) DnsRecordActions {
+func (d *dns) With(domain string, serviceID int) DnsRecordActions {
 	return &domainAction{
-		h:   d.h,
-		dom: domain,
+		h:     d.h,
+		dom:   domain,
+		svcID: serviceID,
 	}
 }
 
 type domainAction struct {
-	h   helper
-	dom string
+	h     helper
+	dom   string
+	svcID int
 }
 
-func (d *domainAction) Create(recType DnsRecordType, r *DnsRecord) ApiError {
-	return apiErr(d.change(http.MethodPost, string(recType), r))
+func (d *domainAction) ListAll() ([]DnsRecord, ApiError) {
+	return d.List("", "")
 }
 
-func (d *domainAction) change(method string, sub string, r *DnsRecord) (*http.Response, error) {
-	data, err := json.Marshal(r)
+func (d *domainAction) List(recType DnsRecordType, recName string) ([]DnsRecord, ApiError) {
+	// Get first page
+	allRecords, nextPageUrl, nextPage, err := d.ListPage(recType, recName, "", 0)
 	if err != nil {
 		return nil, err
 	}
-	return d.h.do(method, fmt.Sprintf("dns/%s/%s/v1", d.dom, sub), bytes.NewBuffer(data))
+
+	// Keep getting next pages while we have either nextPageUrl or nextPage
+	pageCount := 1
+	for (nextPageUrl != "" || nextPage > 0) && pageCount < d.h.maxPages {
+		pageCount++
+		var pageRecords []DnsRecord
+		pageRecords, nextPageUrl, nextPage, err = d.ListPage(recType, recName, nextPageUrl, nextPage)
+		if err != nil {
+			return nil, err
+		}
+		allRecords = append(allRecords, pageRecords...)
+	}
+	if pageCount >= d.h.maxPages {
+		return nil, apiErr(nil, fmt.Errorf("maximum page limit reached in List, maxPages: %d, increase the limit in the configuration", d.h.maxPages))
+	}
+	return allRecords, nil
 }
 
-func (d *domainAction) List() ([]DnsRecord, ApiError) {
-	resp, err := d.h.do(http.MethodGet, fmt.Sprintf("dns/%s/records/v1", d.dom), nil)
+func (d *domainAction) ListPage(recType DnsRecordType, recName string, recPageUrl string, recPage int) ([]DnsRecord, string, int, ApiError) {
+	ret, err := d.ListPaginated(recType, recName, recPageUrl, recPage)
 	if err != nil {
-		return nil, apiErr(nil, err)
+		return nil, "", 0, err
+	}
+
+	var nextPageUrl string
+	var nextPage int
+
+	switch {
+	// Get the next page request paramater (query string) from the field nextPageUrl of the last response - preferred
+	case ret.NextPageUrl != nil && *ret.NextPageUrl != "":
+		// Typical response field nextPageUrl: "/?page=2", strip all leading "/?" characters from it
+		nextPageUrl = strings.TrimLeft(*ret.NextPageUrl, "/?")
+	// Calculate the next page from the currentPage and totalPages fields of the last response - only if not set via nextPageUrl
+	case ret.CurrentPage != nil && ret.TotalPages != nil && *ret.CurrentPage < *ret.TotalPages:
+		nextPage = *ret.CurrentPage + 1
+	}
+	return ret.Data, nextPageUrl, nextPage, err
+}
+
+func (d *domainAction) ListPaginated(recType DnsRecordType, recName string, recPageUrl string, recPage int) (DnsRecordPaginatedCollection, ApiError) {
+	// HTTP request params
+	reqParams := url.Values{}
+	reqParams.Add("descending", "false")
+	reqParams.Add("sortBy", "name")
+	if recType != "" {
+		reqParams.Add("filters[type]", fmt.Sprintf("[\"%s\"]", recType))
+	}
+	if recName != "" {
+		reqParams.Add("filters[name]", recName)
+	}
+	var ret DnsRecordPaginatedCollection
+	// Requested page obtained from the nextPageUrl field of the last response - preferred
+	hasRecPageUrl := false
+	if recPageUrl != "" {
+		recPageUrlValues, err := url.ParseQuery(recPageUrl)
+		if err != nil {
+			return ret, apiErr(nil, err)
+		}
+		for k, v := range recPageUrlValues {
+			if k != "" && len(v) > 0 && v[0] != "" {
+				reqParams.Add(k, v[0])
+				hasRecPageUrl = true
+			}
+			break
+		}
+	}
+	// Requested page calculated from the currentPage and totalPages fields of the last response - only if not set via nextPageUrl
+	if !hasRecPageUrl && recPage > 1 {
+		reqParams.Add("page", strconv.Itoa(recPage))
+	}
+
+	resp, err := d.h.doWithParams(http.MethodGet, fmt.Sprintf("v2/service/%d/dns/record", d.svcID), reqParams, nil)
+	if err != nil {
+		return ret, apiErr(nil, err)
 	}
 	defer func(b io.ReadCloser) {
 		_ = b.Close()
 	}(resp.Body)
 	if resp.StatusCode > 399 && resp.StatusCode < 600 {
-		return nil, apiErr(resp, fmt.Errorf("invalid response from api: %d", resp.StatusCode))
+		return ret, apiErr(resp, fmt.Errorf("invalid response from API: %d", resp.StatusCode))
 	}
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, apiErr(resp, err)
+		return ret, apiErr(resp, err)
 	}
-	ret := make([]DnsRecord, 0)
+	//ret := make([]DnsRecord, 0)
 	err = json.Unmarshal(body, &ret)
 	if err != nil {
-		return nil, apiErr(resp, err)
+		return ret, apiErr(resp, err)
 	}
 	return ret, apiErr(resp, nil)
 }
 
-func (d *domainAction) Update(recType DnsRecordType, r *DnsRecord) ApiError {
-	return apiErr(d.change(http.MethodPut, string(recType), r))
+func (d *domainAction) Create(r *DnsRecord) ApiError {
+	data, err := json.Marshal(r)
+	if err != nil {
+		return apiErr(nil, err)
+	}
+	return apiErr(d.h.do(http.MethodPost, fmt.Sprintf("v2/service/%d/dns/record", d.svcID), bytes.NewBuffer(data)))
 }
 
-func (d *domainAction) Delete(hashId string) ApiError {
-	return apiErr(d.h.do(http.MethodDelete, fmt.Sprintf("/dns/%s/%s/v1", d.dom, hashId), nil))
+func (d *domainAction) change(method string, recordID int, r *DnsRecord) (*http.Response, error) {
+	data, err := json.Marshal(r)
+	if err != nil {
+		return nil, err
+	}
+	return d.h.do(method, fmt.Sprintf("v2/service/%d/dns/record/%d", d.svcID, recordID), bytes.NewBuffer(data))
+}
+
+func (d *domainAction) Update(ID int, r *DnsRecord) ApiError {
+	return apiErr(d.change(http.MethodPut, ID, r))
+}
+
+func (d *domainAction) Delete(ID int) ApiError {
+	return apiErr(d.h.do(http.MethodDelete, fmt.Sprintf("v2/service/%d/dns/record/%d", d.svcID, ID), nil))
 }
--- go.mod
+++ go.mod
@@ -12,18 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-module github.com/rkosegi/active24-go
+module github.com/hostalp/active24-go
 
-go 1.21
+go 1.24
 
-require (
-	github.com/stretchr/testify v1.10.0
-	k8s.io/klog/v2 v2.130.1
-)
+require k8s.io/klog/v2 v2.130.1
 
-require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/go-logr/logr v1.4.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
-)
+require github.com/go-logr/logr v1.4.3 // indirect
